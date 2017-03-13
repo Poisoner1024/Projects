c4331c27a26e082373b23efdab0537230f845c57
Yunfei Xu (yunfxu) <yunfxu@cisco.com>
2017-03-13 11:41:13 +0800

    remove error committed path.

--- design-pattern/src/com/poisoner/design/pattern/creational/Direction.java
+++
@@ -1,8 +0,0 @@
-package com.poisoner.design.pattern.creational;
-
-/**
- * Created by shenjuan on 2017/3/13.
- */
-public enum Direction {
-    North, South, East, West
-}
--- design-pattern/src/com/poisoner/design/pattern/creational/MapSite.java
+++
@@ -1,8 +0,0 @@
-package com.poisoner.design.pattern.creational;
-
-/**
- * Created by shenjuan on 2017/3/13.
- */
-public abstract class MapSite {
-    public abstract void Enter();
-}
